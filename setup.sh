fac180c6c6c13490d768571c0c195f372746319b
saurabh <50881320+T3CHMYTH@users.noreply.github.com>
2020-08-18 18:54:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #/usr/bin/bash
 
 cp -rf matrix $PREFIX/bin
+cp -rf delmatrix $PREFIX/bin
+chmod 777 $PREFIX/bin/delmatrix
 chmod 777 $PREFIX/bin/matrix
 mv ../python3-matrix $PREFIX/share
 cd $HOME
 echo -e "\e[3;33mMatrix Is Sucessfully installed on Your System\nType matrix to run the martrix\e[0m"
 echo -e "\e[2;31mYou need to run \e[2;34mpip install colored \e[0m\e[2;31mbefore you run the matrix\e[0m"
+echo -e "\e[2;32mTo delete the matrix you can type delmatrix in you terminal\e[0m"
